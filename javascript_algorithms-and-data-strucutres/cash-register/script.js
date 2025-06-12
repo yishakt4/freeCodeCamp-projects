9b4b8b5f01575b9c4810a5da46570b55b715bd37
yishakt4 <halieseliase1936@gmail.com>
2025-06-12 23:45:36 +0300

    hope

---
+++ javascript_algorithms-and-data-strucutres/cash-register/script.js
@@ -0,0 +1,15 @@
+function intToRoman(num) {
+    if (num <= 0) return "Roman numerals do not support zero or negative numbers.";
+    if (num > 3999) return "This converter supports numbers only up to 3999.";
+
+    const romanMap = [
+      { value: 100, symbol: "One Hundred Dollars" },
+      { value: 20, symbol: "Twenty Dollars" },
+      { value: 10, symbol: "Ten Dollars" },
+      { value: 5, symbol: "Five Dollars" },
+      { value: 1, symbol: "Dollar" },
+      { value: 0.25, symbol: "Quarter" },
+      { value: 0.1, symbol: "Dime" },
+      { value: 0.05, symbol: "Nickel" },
+      { value: 0.01, symbol: "Penny" }
+    
